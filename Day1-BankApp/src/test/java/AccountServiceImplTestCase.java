ddf3edf691fae5b1e238f2113523987099e6747b
jvithana <jvithana@chnsipdt0t641.corp.capgemini.com>
2017-03-29 10:20:41 +0530

    commit it

---
+++ Day1-BankApp/src/test/java/AccountServiceImplTestCase.java
@@ -0,0 +1,16 @@
+import org.cap.dto.Account;
+import org.cap.exception.InsufficientBalanceException;
+
+public class AccountServiceImplTestCase {
+	/*public void withdraw(int accountNo, double amount) throws InsufficientBalanceException {
+		A	ccount account=accountDao.findAccountById(accountNo);
+		
+		if(account.getAmount()<amount)
+			throw new InsufficientBalanceException();
+		
+		account.setAmount(account.getAmount()-amount);
+		
+		
+		return account;
+	}*/
+}
