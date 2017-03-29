3ff6d1ecff338139834a61ef3420f0b90e6fae04
jvithana <jvithana@chnsipdt0t641.corp.capgemini.com>
2017-03-29 10:28:29 +0530

    test committ

--- Day1-BankApp/src/main/java/org/cap/dao/AccountDao.java
+++ Day1-BankApp/src/main/java/org/cap/dao/AccountDao.java
@@ -4,7 +4,7 @@ import org.cap.dto.Account;
 import org.cap.dto.Customer;
 
 public interface AccountDao {
-	
+	/*new comments*/
 	public boolean createAccount(Account account);
 	
 	public Account findAccountById(int accountNo);
